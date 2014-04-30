e7f73ff25350a2302a59ff71f4244d3473813e29
pofider <honza.pofider@seznam.cz>
2014-04-30 10:51:41 +0200

    first public page release

---
+++ public/js/custom.js
@@ -0,0 +1,64 @@
+﻿$(function() {
+    
+    $("#settings").on('click', function() {
+
+        $("#settings-expand").toggle("slow");
+    });
+    
+    
+    $(".dropdown-menu li").click(function() {
+        $(this).parent().prev().text($(this).text());
+        refresh();
+        refreshUrl();
+    });
+    
+    function refreshUrl() {
+        var urlFormat = $("#url-format").prev().html();
+        var urlOrientation = $("#url-orientation").prev().html();
+        $("#url-format-input").attr("value",urlFormat);
+        $("#url-orientation-input").attr("value",urlOrientation);
+    }
+
+
+    function refresh() {
+        var pageFormat = $("#page-format").prev().html();
+        var pageOrientation = $("#page-orientation").prev().html();
+        var color = $("#color").prev().html();
+        
+        function getSettingsAttributes() {
+            return " data-format=&quot;" + pageFormat + "&quot;" +
+                " data-orientation=&quot;" + pageOrientation + "&quot;" +
+                " data-color=&quot;" + color + "&quot;";
+        }
+
+        function getCode() {
+            return "&lt;div id=&quot;jsr-root&quot;" + getSettingsAttributes() + " &gt;&lt;/div&gt;<br/>\n" +
+                "&lt;script&gt;<br/>\n" +
+                "(function (d, s, id) {<br/>\n" +
+                "  var js, fjs = d.getElementsByTagName(s)[0];<br/>\n" +
+                "  if (d.getElementById(id)) return;<br/>\n" +
+                "  js = d.createElement(s); js.id = id;<br/>\n" +
+                "  js.src = &quot;//pagetopdf.com/js/widget.js&quot;;<br/>\n" +
+                "  fjs.parentNode.insertBefore(js, fjs);<br/>\n" +
+                "  }(document, 'script', 'jsr-pagetopdf'));<br/>\n" +
+                "&lt;/script&gt;";
+        }
+
+        function refreshWidget() {
+            $("#jsr-root").html("");
+            
+            $("#jsr-root").attr("data-format", pageFormat);
+            $("#jsr-root").attr("data-orientation", pageOrientation);
+            $("#jsr-root").attr("data-color", color);
+
+            paintPageToPdfButton();
+        }
+        
+        $("#embed-code").removeClass("prettyprinted");
+        $("#embed-code").html(getCode());
+        prettyPrint();
+        refreshWidget();
+    }
+
+    refresh();
+});
---
+++ public/js/widget.js
@@ -0,0 +1,29 @@
+﻿function paintPageToPdfButton() {
+    var jsrRoot = document.getElementById("jsr-root");
+
+    var color = jsrRoot.getAttribute("data-color");
+    //jsrRoot.innerHTML
+    var html = 
+    "<form target='_blank' method='POST' enctype='multipart/form-data' action='https://playground.jsreport.net/api/report' id='jsrForm' >" + 
+    "<input hidden='true' type='text' id='jsrUrl' name='template[phantom[url]' />" +
+    "<input hidden='true' type='text' id='jsrFormat' name='template[phantom[format]' />" +
+        "<input hidden='true' type='text' id='jsrOrientation' name='template[phantom[orientation]' />" +
+    "<input hidden='true' type='text' name='template[recipe]' value='phantom-pdf' />" +
+    "<input hidden='true' type='text' name='template[content]' value='' />" +
+    "<div><button id='jsrPrint' type='submit' style='background-color:" + color + "; position:relative; padding-left:45px;padding-right:12px; padding-top:5px;padding-bottom:5px;color:white;border: 0 none;'>Print to Pdf" +
+    "<img  src='http://137.117.246.16:882/img/pdf-ico.png' style='margin-left:-4px;position: absolute; left: 0px; top: 0px; height:100%'></button></div>" +
+    "</form>";
+    
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute("frameborder", "0");
+    jsrRoot.appendChild(iframe);
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(html);
+    iframe.contentWindow.document.close();
+    
+    iframe.contentWindow.document.getElementById('jsrUrl').value = window.location.href;
+    iframe.contentWindow.document.getElementById('jsrFormat').value = jsrRoot.getAttribute("data-format");
+    iframe.contentWindow.document.getElementById('jsrOrientation').value = jsrRoot.getAttribute("data-orientation");
+}
+
+paintPageToPdfButton();
